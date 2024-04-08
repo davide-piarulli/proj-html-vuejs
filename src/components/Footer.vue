6bfdc0e594f9a636430c089b0d77f2a7ff567a7f
davide-piarulli <davide.piarulli.1@gmail.com>
2024-04-08 11:10:11 +0100

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script>
+export default {};
+</script>
+
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from "../data/store";
+export default {};
+</script>
+
+<template>
+  <header>
+    <nav class="navbar">
+      <div class="container">
+        <p>questa è l'header</p>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header{
+  background-color: #21333e;
+  color: #bcbdbd;
+}
+</style>
