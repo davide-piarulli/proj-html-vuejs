0a207764bd9192d01bec3a395a5c53b4553cbb56
davide-piarulli <davide.piarulli.1@gmail.com>
2024-04-08 11:31:47 +0100

    [add] static header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,14 +7,40 @@ export default {};
   <header>
     <nav class="navbar">
       <div class="container">
-        <p>questa è l'header</p>
+        <div class="row w-100">
+          <!-- LEFT COLUMN -->
+          <div class="col-6 d-flex p-2">
+            <i class="fa-solid fa-clock"></i>
+            <p>Open Hours: Mon - Sat - 9:00 - 18:00</p>
+          </div>
+          <!-- /LEFT COLUMN -->
+
+          <!-- RIGHT COLUMN -->
+          <div
+            class="col-6 d-flex justify-content-end align-content-center p-2"
+          >
+            <div class="phone d-flex px-4">
+              <i class="fa-solid fa-phone"></i>
+              <span>+1 (305) 1234-5678</span>
+            </div>
+            <div class="email d-flex px-4">
+              <span>hello@example.com</span>
+            </div>
+            <div class="social-icons">
+              <i class="fa-brands fa-facebook-f px-3"></i>
+              <i class="fa-brands fa-x-twitter px-3"></i>
+              <i class="fa-brands fa-linkedin-in px-3"></i>
+            </div>
+          </div>
+        </div>
+        <!-- /RIGHT COLUMN -->
       </div>
     </nav>
   </header>
 </template>
 
 <style lang="scss" scoped>
-header{
+header {
   background-color: #21333e;
   color: #bcbdbd;
 }
