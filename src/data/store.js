a824cde6f2005553446fd651cca755106bd1b249
davide-piarulli <davide.piarulli.1@gmail.com>
2024-04-08 21:45:41 +0100

    [add] array header and footer in store

--- src/data/store.js
+++ src/data/store.js
@@ -1,3 +1,146 @@
 import { reactive } from "vue";
 
-export const store = reactive({});
+export const store = reactive({
+  headerNav: [
+    {
+      title: "Home",
+      url: "#",
+    },
+    {
+      title: "About",
+      url: "#",
+    },
+    {
+      title: "Services",
+      url: "#",
+    },
+    {
+      title: "Process",
+      url: "#",
+    },
+    {
+      title: "Testimonials",
+      url: "#",
+    },
+  ],
+
+  contactInfo: [
+    {
+      address: "Main Avenue, 987",
+    },
+    {
+      phone: "+1 (305) 1234-5678",
+    },
+    {
+      email: "hello@example.com",
+    },
+  ],
+
+  socialIcons: [
+    {
+      name: '<i class="fa-brands fa-facebook-f"></i>',
+      href: "#",
+    },
+    {
+      name: '<i class="fa-brands fa-x-twitter"></i>',
+      href: "#",
+    },
+    {
+      name: '<i class="fa-brands fa-linkedin-in"></i>',
+      href: "#",
+    },
+  ],
+
+  footerNav: [
+    {
+      title: "About",
+      url: "#",
+      submenu: [
+        {
+          title: "The Company",
+          url: "#",
+        },
+        {
+          title: "Institutional",
+          url: "#",
+        },
+        {
+          title: "Social & Events",
+          url: "#",
+        },
+        {
+          title: "Innovation",
+          url: "#",
+        },
+        {
+          title: "Environment",
+          url: "#",
+        },
+        {
+          title: "Technology",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Transport",
+      url: "#",
+      submenu: [
+        {
+          title: "Industrialized",
+          url: "#",
+        },
+        {
+          title: "Chemicals",
+          url: "#",
+        },
+        {
+          title: "Packaged Liquids",
+          url: "#",
+        },
+        {
+          title: "Construction",
+          url: "#",
+        },
+        {
+          title: "Laminated Wood",
+          url: "#",
+        },
+        {
+          title: "And others",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Support",
+      url: "#",
+      submenu: [
+        {
+          title: "Responsibility",
+          url: "#",
+        },
+        {
+          title: "Term of Use",
+          url: "#",
+        },
+        {
+          title: "About Cookies",
+          url: "#",
+        },
+        {
+          title: "Privacy Policy",
+          url: "#",
+        },
+        {
+          title: "Accessibility",
+          url: "#",
+        },
+        {
+          title: "Information",
+          url: "#",
+        },
+      ],
+    },
+  ],
+});
