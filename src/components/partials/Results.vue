9a2b161e11eca9d752496ed0c0017411baa00631
davide-piarulli <davide.piarulli.1@gmail.com>
2024-04-10 11:19:43 +0100

    [add] results static section

--- src/components/partials/Results.vue
+++ src/components/partials/Results.vue
@@ -4,7 +4,37 @@ export default {};
 
 <template>
   <section id="results">
-    <h1>RESULTS</h1>
+    <div class="container-xl">
+      <div class="row main">
+        <div class="col text-center">
+          <span class="section-title">WHAT ARE WE DOING</span>
+          <div class="title">
+            <h2>
+              <span>Results</span>
+              in Numbers
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div class="row bottom">
+        <div class="col text-center">
+          <span>128</span>
+          <h3>Certifications</h3>
+        </div>
+        <div class="col text-center">
+          <span>230</span>
+          <h3>Employees</h3>
+        </div>
+        <div class="col text-center">
+          <span>517</span>
+          <h3>Customers</h3>
+        </div>
+        <div class="col text-center">
+          <span>94</span>
+          <h3>Countries Served</h3>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -13,5 +43,37 @@ export default {};
   background-color: #111117;
   height: 585px;
   color: white;
+  background-image: url(../../assets/img/bg-10.jpg);
+  background-position: center;
+  .row.main {
+    padding: 125px 0 50px;
+    .section-title {
+      color: #058283;
+      font-weight: 500;
+    }
+    .title {
+      h2 {
+        margin-top: 10px;
+        font-size: 3.5rem;
+        font-weight: 700;
+        span {
+          color: white;
+          background-color: #058283;
+          padding: 0px 10px;
+          border-radius: 5px;
+        }
+      }
+    }
+  }
+  .row.bottom {
+    .col {
+      position: relative;
+    }
+    span {
+      font-size: 2.8rem;
+      color: #00a6a6;
+      font-weight: 600;
+    }
+  }
 }
 </style>
